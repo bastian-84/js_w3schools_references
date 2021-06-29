eb09bbf7557194013e9569a5a08e2e4068f11d56
Sebastian Reichenbach <reichenbach.s@gmail.com>
2021-06-28 18:36:52 -0700

    SR: next: DOM Events ctrlKey

--- public/js/dom-events-1.js
+++ public/js/dom-events-1.js
@@ -2,8 +2,8 @@
 // ########################## Hilfsfunktionen ########################## //
 // ##################################################################### //
 var fnGetEl = (param1) => {
-    //return document.getElementById(param1);
-    return document.querySelector(param1);
+  //return document.getElementById(param1);
+  return document.querySelector(param1);
 }
 
 // ##################################################################### //
@@ -63,7 +63,7 @@ let fnOnBeforePrint = () => {
     alert("You are about to print this document!");
 }
  */
- 
+
 
 // ====================================================== //
 // ================== xxx ================== //
@@ -74,7 +74,7 @@ let fnDoX1 = () => {
     sOutput01.innerHTML = 'Selection: ' + x1;
 }
  */
- 
+
 
 // ====================================================== //
 // ================== xxx ================== //
@@ -101,23 +101,23 @@ sB1.addEventListener('click', fnDoX1);
 // ====================================================== //
 
 function dragStart(event) {
-    event.dataTransfer.setData("Text", event.target.id);
-  }
-  
-  function dragging(event) {
-    document.getElementById("demo").innerHTML = "The p element is being dragged";
-  }
-  
-  function allowDrop(event) {
-    event.preventDefault();
-  }
-  
-  function drop(event) {
-    event.preventDefault();
-    var data = event.dataTransfer.getData("Text");
-    event.target.appendChild(document.getElementById(data));
-    document.getElementById("demo").innerHTML = "The p element was dropped";
-  }
+  event.dataTransfer.setData("Text", event.target.id);
+}
+
+function dragging(event) {
+  document.getElementById("demo").innerHTML = "The p element is being dragged";
+}
+
+function allowDrop(event) {
+  event.preventDefault();
+}
+
+function drop(event) {
+  event.preventDefault();
+  var data = event.dataTransfer.getData("Text");
+  event.target.appendChild(document.getElementById(data));
+  document.getElementById("demo").innerHTML = "The p element was dropped";
+}
 
 
 // ====================================================== //
@@ -170,7 +170,7 @@ sB1.addEventListener('click', fnDoX1);
 // ====================================================== //
 // ================== xxx ================== //
 // ====================================================== //
- /* 
+/* 
 if(typeof(EventSource) !== "undefined") {
   var source = new EventSource("/html/demo_sse.php");
   source.onopen = function() {
@@ -209,7 +209,7 @@ sB1.addEventListener('click', fnDoX1);
 // ====================================================== //
 // ================== xxx ================== //
 // ====================================================== //
-
+/* 
 let fnDoX1 = () => {
   let iW = window.outerWidth;
   let iH = window.outerHeight;
@@ -217,14 +217,26 @@ let fnDoX1 = () => {
   sOutput01.innerHTML = sOutput;
 }
 sB1.addEventListener('click', fnDoX1);
+ */
 
+// ====================================================== //
+// ================== xxx ================== //
+// ====================================================== //
+/* 
+let x1 = 0;
+let fnDoX1 = () => {
+  sOutput01.innerHTML = x1 += 1;
+}
+sB1.addEventListener('click', fnDoX1);
+  */
 
 // ====================================================== //
 // ================== xxx ================== //
 // ====================================================== //
 /* 
 let fnDoX1 = () => {
-
+  let x1 = fnGetEl('#myInput');
+  sOutput01.innerHTML = x1.value;
 }
 sB1.addEventListener('click', fnDoX1);
  */
@@ -234,10 +246,408 @@ sB1.addEventListener('click', fnDoX1);
 // ====================================================== //
 /* 
 let fnDoX1 = () => {
+  alert("You selected some text!");
+}
+sB1.addEventListener('click', fnDoX1);
+  */
 
+// ====================================================== //
+// ================== xxx ================== //
+// ====================================================== //
+
+/* 
+let fnDoX1 = () => {
+  this.innerHTML = "transitionend event occured - The transition has completed";
+  this.style.backgroundColor = "pink";
+}
+
+document.getElementById('myDIV443').addEventListener('webkitTransitionEnd', fnDoX1);
+document.getElementById('myDIV443').addEventListener('transitionend', fnDoX1);
+
+sB1.addEventListener('click', fnDoX1);
+ */
+
+// ====================================================== //
+// ================== xxx ================== //
+// ====================================================== //
+/* 
+let w1 = fnGetEl('#myDIV221');
+
+let fnDoX1 = () => {
+  console.log('fn called');
+  w1.style.fontSize = "35px";     // NOT USING THIS, VAR NAME NEEDED
+}
+
+w1.addEventListener('wheel', fnDoX1);
+sB1.addEventListener('click', fnDoX1);
+ */
+
+// ====================================================== //
+// ================== xxx ================== //
+// ====================================================== //
+/* 
+let isKeyPressed = (event) => {
+  if (event.altKey) {
+    alert("The ALT key was pressed!");
+  } else {
+    alert("The ALT key was NOT pressed!");
+  }
+}
+ */
+
+
+// ====================================================== //
+// ================== xxx ================== //
+// ====================================================== //
+/* 
+let x11 = fnGetEl('#myDIV558');
+
+
+let myStartFunction = (event) => {
+  console.log('fn called');
+  x11.innerHTML = "The animation-name is: " + event.animationName;
+}
+
+
+// Code for Chrome, Safari and Opera
+x11.addEventListener("webkitAnimationStart", myStartFunction);
+
+// Standard syntax
+x11.addEventListener("animationstart", myStartFunction);
+
+ */
+
+
+// ====================================================== //
+// ================== xxx ================== //
+// ====================================================== //
+
+
+/* 
+let fnDoX1 = (paramEvent) => {
+  let x1 = paramEvent.bubbles;
+  sOutput01.innerHTML = x1;
+
+}
+//sB1.addEventListener('click', fnDoX1);
+ */
+
+
+
+// ====================================================== //
+// ================== xxx ================== //
+// ====================================================== //
+/* 
+let fnDoX1 = (paramEvent) => {
+  alert('pressed ' + paramEvent.button);
 }
 sB1.addEventListener('click', fnDoX1);
  */
+
+// ====================================================== //
+// ================== xxx ================== //
+// ====================================================== //
+/* 
+let fnDoX1 = (paramEvent) => {
+  let x1 = paramEvent.cancelable;
+  sOutput01.innerHTML = x1;
+}
+sB1.addEventListener('click', fnDoX1);
+ */
+
+// ====================================================== //
+// ================== xxx ================== //
+// ====================================================== //
+/* 
+let fnDoX1 = (paramEvent) => {
+  let x11 = paramEvent.charCode;
+  sOutput01.innerHTML = x11;
+}
+sB1.addEventListener('click', fnDoX1);
+ */
+
+// ====================================================== //
+// ================== xxx ================== //
+// ====================================================== //
+/* 
+let fnDoX1 = (paramEvent) => {
+  var x = paramEvent.clientX;
+  var y = paramEvent.clientY;
+  var coords = "X coords: " + x + ", Y coords: " + y;
+  sOutput01.innerHTML = coords;
+}
+sB1.addEventListener('click', fnDoX1);
+ */
+
+// ====================================================== //
+// ================== xxx ================== //
+// ====================================================== //
+/* 
+let fnDoX1 = (pamEvent) => {
+  let x1 = pamEvent.code;
+  sOutput01.innerHTML = x1;
+}
+sB1.addEventListener('click', fnDoX1);
+ */
+
+// ====================================================== //
+// ================== xxx ================== //
+// ====================================================== //
+/* 
+let div556 = fnGetEl('#div556');
+
+let fnDoX1 = () => {
+
+  div556.innerHTML += '*';
+
+  var x = document.createEvent("MouseEvent");
+  x.initMouseEvent("mouseover", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+
+  sOutput01.dispatchEvent(x);
+}
+
+//sB1.addEventListener('click', fnDoX1);
+div556.addEventListener('mouseenter', fnDoX1);
+ */
+// ====================================================== //
+// ================== xxx ================== //
+// ====================================================== //
+/* 
+let fnDoX1 = () => {
+  let x1 = document.defaultView;
+  sOutput01.innerHTML = x1;
+}
+sB1.addEventListener('click', fnDoX1);
+ */
+
+// ====================================================== //
+// ================== xxx ================== //
+// ====================================================== //
+/* 
+let fnDoX1 = () => {
+  let x1 = document.doctype.name;
+  sOutput01.innerHTML = x1;
+}
+sB1.addEventListener('click', fnDoX1);
+ */
+// ====================================================== //
+// ================== xxx ================== //
+// ====================================================== //
+/* 
+let fnDoX1 = () => {
+  let x1 = document.documentElement.nodeName;
+  sOutput01.innerHTML = x1;
+}
+sB1.addEventListener('click', fnDoX1);
+ */
+// ====================================================== //
+// ================== xxx ================== //
+// ====================================================== //
+/* 
+let fnDoX1 = () => {
+  let x1 = document.documentMode;
+  sOutput01.innerHTML = x1;
+}
+sB1.addEventListener('click', fnDoX1);
+ */
+
+// ====================================================== //
+// ================== xxx ================== //
+// ====================================================== //
+/* 
+let fnDoX1 = () => {
+  let x1 = document.documentURI;
+  sOutput01.innerHTML = x1;
+}
+sB1.addEventListener('click', fnDoX1);
+ */
+// ====================================================== //
+// ================== xxx ================== //
+// ====================================================== //
+/* 
+let fnDoX1 = () => {
+  let x1 = document.domain;
+  sOutput01.innerHTML = x1;
+}
+sB1.addEventListener('click', fnDoX1);
+ */
+// ====================================================== //
+// ================== xxx ================== //
+// ====================================================== //
+/* 
+let fnDoX1 = () => {
+  let x1 = document.embeds.length;
+  sOutput01.innerHTML = x1;
+}
+sB1.addEventListener('click', fnDoX1);
+ */
+
+// ====================================================== //
+// ================== xxx ================== //
+// ====================================================== //
+/* 
+document.designMode = "on";
+
+let fnDoX1 = (prmEvent) => {
+  if(prmEvent.keyCode == 16){
+    document.execCommand('bold');
+  }
+}
+sB1.addEventListener('click', fnDoX1);
+ */
+
+// ====================================================== //
+// ================== xxx ================== //
+// ====================================================== //
+/* 
+let fnDoX1 = () => {
+  let x1 = document.forms.length;
+  sOutput01.innerHTML = x1;
+}
+sB1.addEventListener('click', fnDoX1);
+ */
+
+// ====================================================== //
+// ================== xxx ================== //
+// ====================================================== //
+/* 
+let s444 = fnGetEl('#demo444');
+
+let fnDoX1 = () => {
+  if(document.hasFocus()){
+    sOutput01.innerHTML = 'has focus';
+  }else{
+    sOutput01.innerHTML = 'has no focus';
+  }
+}
+sB1.addEventListener('click', fnDoX1);
+
+setInterval(fnDoX1, 1);
+ */
+/* 
+// ====================================================== //
+// ================== xxx ================== //
+// ====================================================== //
+
+let fnDoX1 = () => {
+  
+  let x1 = document.images.length;
+  sOutput01.innerHTML = x1;
+}
+sB1.addEventListener('click', fnDoX1);
+ */
+
+
+// ====================================================== //
+// ================== xxx ================== //
+// ====================================================== //
+/* 
+let fnDoX1 = () => {
+  var imp = document.implementation;
+  sOutput01.innerHTML = imp.hasFeature("HTML", "1.0");
+}
+sB1.addEventListener('click', fnDoX1);
+ */
+
+// ====================================================== //
+// ================== xxx ================== //
+// ====================================================== //
+/* 
+let fnDoX1 = () => {
+  let x1 = document.inputEncoding;
+  sOutput01.innerHTML = x1;
+}
+sB1.addEventListener('click', fnDoX1);
+ */
+
+// ====================================================== //
+// ================== xxx ================== //
+// ====================================================== //
+/* 
+let fnDoX1 = () => {
+  let x1 = document.lastModified;
+  sOutput01.innerHTML = x1;
+}
+sB1.addEventListener('click', fnDoX1);
+ */
+// ====================================================== //
+// ================== xxx ================== //
+// ====================================================== //
+/* 
+let fnDoX1 = () => {
+  let x1 = document.links.length;
+  sOutput01.innerHTML = x1;
+}
+sB1.addEventListener('click', fnDoX1);
+ */
+
+// ====================================================== //
+// ================== xxx ================== //
+// ====================================================== //
+/* 
+let b1111 = fnGetEl('#b1111');
+let b2222 = fnGetEl('#b2222');
+
+let fnAddTextNode = () => {
+  var x = document.createTextNode(" Click again.");
+  var y = document.body;
+  y.appendChild(x);
+  var x2 = document.getElementById("cc");
+  x2.innerHTML = y.childNodes.length;
+}
+let fnNormPara = () => {
+  document.normalize();
+  var x = document.body;
+  var y = document.getElementById("cc");
+  y.innerHTML = x.childNodes.length;
+}
+b1111.addEventListener('click', fnAddTextNode);
+b2222.addEventListener('click', fnNormPara);
+  */
+
+// ====================================================== //
+// ================== xxx ================== //
+// ====================================================== //
+ /* 
+let fnDoX1 = () => {
+  let x1 = document.readyState;
+  sOutput01.innerHTML = x1;
+}
+sB1.addEventListener('click', fnDoX1);
+  */
+
+// ====================================================== //
+// ================== xxx ================== //
+// ====================================================== //
+ /* 
+let fnDoX1 = () => {
+  let x1 = document.scripts.length;
+  sOutput01.innerHTML = x1;
+}
+sB1.addEventListener('click', fnDoX1);
+  */
+
+// ====================================================== //
+// ================== xxx ================== //
+// ====================================================== //
+ /* 
+let fnDoX1 = () => {
+  let x1 = document.title;
+  sOutput01.innerHTML = x1;
+}
+sB1.addEventListener('click', fnDoX1);
+ 
+ */
+// ====================================================== //
+// ================== xxx ================== //
+// ====================================================== //
+ 
+let fnDoX1 = () => {
+  let x1 = document.URL;
+  sOutput01.innerHTML = x1;
+}
+sB1.addEventListener('click', fnDoX1);
+ 
 
 // ====================================================== //
 // ================== xxx ================== //
